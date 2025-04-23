12140f0e5d117b03858128398387f78019521449
Eldareich <eldarkydyrbek@email.com>
2025-04-23 16:25:43 +0500

    add Fiels

--- myproject55/settings.py
+++ myproject55/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts', 
-    'generator', 
+    # 'accounts', 
+    # 'generator', 
 ]
 
 
