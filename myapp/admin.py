c371982385df28d1296f470f86b1b923ae5b26f7
Eldareich <eldarkydyrbek@email.com>
2025-05-21 01:53:43 +0500

    fixs

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Resume
-
-@admin.register(Resume)
-class ResumeAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'email', 'created_at')  
-    search_fields = ('full_name', 'email')  
-    list_filter = ('created_at',)  
-# Register your models here.
